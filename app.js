a4b7e8dd542ff8a773e970b2eeb1ca5ce1dda748
Kanishka Goyal <me200003041@iiti.ac.in>
2022-12-09 13:13:57 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,69 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+const https = require("https");
+const request=require("request");
+
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/signup.html");
+});
+
+app.post("/", function(req,res){
+     const fname=req.body.fname;
+     const sname=req.body.sname;
+     const email=req.body.email;
+
+     const data={
+          members:[{
+            email_address:email,
+            status:"subscribed",
+            merge_fields:{
+                FNAME:fname,
+                LNAME:sname
+            }
+          }
+           
+          ]
+     };
+     const jsonData=JSON.stringify(data);
+     
+     url="https://us14.api.mailchimp.com/3.0/lists/af88ee8c75";
+     const options={
+        method:"POST",
+        auth:"Kanishka:18aa2b924becb3637a0d26496be6a323-us14"
+
+     };
+
+     const request=https.request(url, options, function(response){
+        console.log(response.statusCode);
+        if(response.statusCode === 200) 
+        res.sendFile(__dirname+"/success.html");
+        else res.sendFile(__dirname+"/failure.html");
+
+        response.on("data", function(data){
+            //console.log(JSON.parse(data));
+        });
+     });
+     request.write(jsonData);
+     request.end();
+     
+
+    //  res.send("Hello");
+});
+
+app.post("/failure", function(req,res){
+    res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000,function(){
+    console.log("server listening to port 3000");
+});
+
+// apikey
+// 18aa2b924becb3637a0d26496be6a323-us14
+
+//list id
+//
